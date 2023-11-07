fd446883b4dd7e2fcc5012e8f25453b5d46b2d57
JWrows2014 <wuz8@nih.gov>
2023-11-06 19:45:14 -0500

    ARC donation wait time tracker commit number 2.

---
+++ staff_input/app.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(shinyTime)
+library(tidyverse)
+library(DT)
+library(magrittr)
+library(reactable)
+
+source("staff_input.R")
+
+# Create UI
+
+# Application title
+
+ui <- fluidPage(
+
+titlePanel("Red Cross staff donation information input"),
+
+# Sidebar with a slider input for number of bins 
+sidebarLayout(position="left",
+              fluidRow(
+                       column(width=12, radioButtons("machine_name", "Choose a machine",
+                             choices=c("Whole blood 1"="WB1",
+                                       "Whole blood 2"="WB2",
+                                       "Whole blood 3"="WB3",
+                                       "Platelet 1"="Platelet1",
+                                       "Platelet 2"="Platelet2",
+                                       "Power red 1"="PR1",
+                                       "Power red 2"="PR2",
+                                       "Power red 3"="PR3",
+                                       "None"="None"),selected="None"),
+                             column(width=4, timeInput("start_time", "Start time", second=FALSE),
+                             actionButton(inputId = "submit",label = "Submit start time")        
+              )
+                )),
+              
+              # Show a plot of the generated distribution
+              
+              #DT::dataTableOutput('wait_time')
+              
+              mainPanel(
+                reactableOutput("wait_time"))
+              
+)
+)
+
+server <- function(input, output) {
+  
+  dwt <- reactiveVal(donation_wait_time)
+  observeEvent(input$submit,
+               {dwt(dwt() %>% 
+                    mutate(Start.time=case_when(donation_wait_time$Machine.name==input$machine_name ~ strftime(input$start_time, "%H:%M"), TRUE ~ Start.time)))
+                    #%>% mutate(Finish.time=case_when(donation_wait_time$Donation.type=="Platelet" ~ substr(strptime(donation_wait_time$Start.time, format="%H:%M")+7200,12,20), TRUE ~ Finish.time))
+                    #%>% mutate(Finish.time=case_when(donation_wait_time$Donation.type=="WB" ~ substr(strptime(donation_wait_time$Start.time, format="%H:%M")+1800,12,20), TRUE ~ Finish.time))
+                    #%>% mutate(Finish.time=case_when(donation_wait_time$Donation.type=="PR" ~ substr(strptime(donation_wait_time$Start.time, format="%H:%M")+3600,12,20), TRUE ~ Finish.time)))
+                    donation_wait_time <<- dwt()
+                    write.csv(donation_wait_time,file="~/Documents/arc_donation_wait_time_version2/arc_donation_time_tracker.csv",quote=FALSE,row.names=FALSE)})
+  observeEvent(input$submit,
+               {dwt(dwt() %>% 
+                    mutate(Finish.time=case_when(donation_wait_time$Donation.type=="Platelet" ~ substr(strptime(donation_wait_time$Start.time, format="%H:%M")+7200,12,20), TRUE ~ Finish.time))
+                    %>% mutate(Finish.time=case_when(donation_wait_time$Donation.type=="WB" ~ substr(strptime(donation_wait_time$Start.time, format="%H:%M")+1800,12,20), TRUE ~ Finish.time))
+                    %>% mutate(Finish.time=case_when(donation_wait_time$Donation.type=="PR" ~ substr(strptime(donation_wait_time$Start.time, format="%H:%M")+3600,12,20), TRUE ~ Finish.time)))
+                   donation_wait_time <<- dwt()
+                   write.csv(donation_wait_time,file="~/Documents/arc_donation_wait_time_version2/arc_donation_time_tracker.csv",quote=FALSE,row.names=FALSE)}) 
+
+  output$wait_time <- renderReactable({
+    reactable(
+      dwt()
+    )
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ staff_input/staff_input.R
@@ -0,0 +1,1 @@
+donation_wait_time <- read.csv("~/Documents/arc_donation_wait_time_version2/arc_donation_time_tracker.csv")
